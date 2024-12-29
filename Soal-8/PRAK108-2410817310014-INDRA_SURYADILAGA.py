a9cd3dbc16c7bbc9e5297208d00a7c86e3d28dc6
Indra Suryadilaga <indrasryd@gmail.com>
2024-12-29 17:27:28 +0800

    Update PRAK108-2410817310014-INDRA_SURYADILAGA.py

--- Soal-8/PRAK108-2410817310014-INDRA_SURYADILAGA.py
+++ Soal-8/PRAK108-2410817310014-INDRA_SURYADILAGA.py
@@ -1,9 +1,7 @@
 putaran = 5
 jarak = 14
-
-# Rumus untuk menghitung jari-jari
 jarijari = jarak / (2 * 3.14159 * putaran)
 
 print(f'Diketahui:\nPak dengklek mengelilingi taman = {putaran} Putaran')
-print(f'Jarak tempuh Pak dengklek = {jarak} Kilometer\n')
+print(f'Jarak tempuh Pak dengklek = {jarak} Kilometer')
 print(f'Jawab :\nJari-jari taman yang dikelilingi Pak Dengklek adalah {jarijari:.2f} Kilometer')
