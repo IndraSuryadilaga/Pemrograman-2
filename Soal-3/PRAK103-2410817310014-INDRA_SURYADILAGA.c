bcce419cdcb57f2e90909c4dde3702e405e97e61
Indra Suryadilaga <indrasryd@gmail.com>
2024-12-29 17:18:27 +0800

    Update PRAK103-2410817310014-INDRA_SURYADILAGA.c

--- Soal-3/PRAK103-2410817310014-INDRA_SURYADILAGA.c
+++ Soal-3/PRAK103-2410817310014-INDRA_SURYADILAGA.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    float a = 5;
-    float b = 6;
-    float x = 7;
-    float y = 8;
+    float a = 5, b = 6, x = 7, y = 8;
     
     printf("Variabel a bernilai %.0f\n",a);
     printf("Variabel b bernilai %.0f\n",b);
     printf("Variabel x bernilai %.0f\n",x);
     printf("Variabel y bernilai %.0f\n",y);
-    printf("Jumlah a ditambah b dikali x dibagi y adalah %.4f\n", a+b*x/y);
+    printf("Jumlah a ditambah b dikali x dibagi y adalah %f\n", a+b*x/y);
     return 0;
 }
