eebb266c2dd673e58ff8769d27f0a60d23f984e4
Indra Suryadilaga <indrasryd@gmail.com>
2024-12-29 17:27:09 +0800

    Update PRAK108-2410817310014-INDRA_SURYADILAGA.c

--- Soal-8/PRAK108-2410817310014-INDRA_SURYADILAGA.c
+++ Soal-8/PRAK108-2410817310014-INDRA_SURYADILAGA.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    int putaran = 5;
-    float jarak = 14.0;
-    
-    // Rumus untuk menghitung jari-jari
-    float jarijari = jarak / (2 * 3.14159 * putaran);
+    int putaran = 5, jarak = 14;
+    float jarijari = jarak / (2 * 3.14 * putaran);
     
     printf("Diketahui:\n");
     printf("Pak dengklek mengelilingi taman = %d Putaran\n", putaran);
-    printf("Jarak tempuh Pak dengklek = %.0f Kilometer\n\n", jarak);
+    printf("Jarak tempuh Pak dengklek = %d Kilometer\n", jarak);
     printf("Jawab :\n");
     printf("Jari-jari taman yang dikelilingi Pak Dengklek adalah %.2f Kilometer\n", jarijari);
-    
     return 0;
 }
