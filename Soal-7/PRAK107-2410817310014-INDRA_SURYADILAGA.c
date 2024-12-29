bae0f953cc1fc4d63b968ce1dad0c9ccdfd3539c
Indra Suryadilaga <indrasryd@gmail.com>
2024-12-29 17:25:49 +0800

    Update PRAK107-2410817310014-INDRA_SURYADILAGA.c

--- Soal-7/PRAK107-2410817310014-INDRA_SURYADILAGA.c
+++ Soal-7/PRAK107-2410817310014-INDRA_SURYADILAGA.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    int a = 4;
-    int b = 5;
-    int c = 7;
-    int x = 85000;
-    int keliling = a + b + c;
-    int biaya = keliling * x;
+  int a = 4, b = 5, c = 7;
+  int harga = 85000;
+  int keliling = a + b + c;
+  int biaya = keliling * harga;
 
-    printf("Diketahui :\n");
-    printf("Panjang sisi segitiga berturut turut adalah %d, %d, %d\n", a, b, c);
-    printf("Keliling tanah Pak Dengklek adalah %d\n", keliling);
-    printf("Harga tanah per meter adalah Rp.%d\n", x);
-    printf("Jawab :\n");
-    printf("Biaya yang diperlukan pak Dengklek adalah : Rp.%d\n", biaya);
-
-    return 0;
+  printf("Diketahui :\n");
+  printf("Panjang sisi segitiga berturut turut adalah %d, %d, %d\n", a, b, c);
+  printf("Keliling tanah Pak Dengklek adalah %d\n", keliling);
+  printf("Harga tanah per meter adalah Rp.%d\n", harga);
+  printf("Jawab :\n");
+  printf("Biaya yang diperlukan pak Dengklek adalah : Rp.%d\n", biaya);
 }
