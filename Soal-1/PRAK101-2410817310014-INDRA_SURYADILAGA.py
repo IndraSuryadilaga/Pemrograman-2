812f55d24b9a05734f9dbc87fa960d53eaa9a346
Indra Suryadilaga <indrasryd@gmail.com>
2024-12-29 17:16:49 +0800

    Update PRAK101-2410817310014-INDRA_SURYADILAGA.py

--- Soal-1/PRAK101-2410817310014-INDRA_SURYADILAGA.py
+++ Soal-1/PRAK101-2410817310014-INDRA_SURYADILAGA.py
@@ -1,14 +1,8 @@
-# x = int(input("Masukkan nilai x: "))
-# y = int(input("Masukkan nilai y: "))
-# z = int(input("Masukkan nilai z: "))
-x=5
-y=6
-z=7
+x = 5
+y = 6
+z = 7
 
-charvar = ['x', 'y', 'z']
-datavar = [x, y, z]
-
-for char, data in zip(charvar, datavar):
-    print(f'Variabel {char} bernilai {data}')
-
+print(f"Variabel x bernilai {x}")
+print(f"Variabel y bernilai {y}")
+print(f"Variabel z bernilai {z}")
 print(f'Jumlah variabel tersebut adalah {x+y+z}')
