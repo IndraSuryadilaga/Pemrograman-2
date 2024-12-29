39b7b99da41bc98581da18578132692621af0c2b
Indra Suryadilaga <indrasryd@gmail.com>
2024-12-29 17:17:34 +0800

    Update PRAK102-2410817310014-INDRA_SURYADILAGA.c

--- Soal-2/PRAK102-2410817310014-INDRA_SURYADILAGA.c
+++ Soal-2/PRAK102-2410817310014-INDRA_SURYADILAGA.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    float a = 5;
-    float b = 6;
-    float c = 7;
+    int a = 5;
+    int b = 6;
+    int c = 7;
     
-    printf("Variabel a bernilai %.0f\n",a);
-    printf("Variabel b bernilai %.0f\n",b);
-    printf("Variabel c bernilai %.0f\n",c);
-    printf("Jumlah a dikali b dibagi c adalah %.4f\n", a*b/c);
+    printf("Variabel a bernilai %d\n",a);
+    printf("Variabel b bernilai %d\n",b);
+    printf("Variabel c bernilai %d\n",c);
+    printf("Jumlah a dikali b dibagi c adalah %f\n", (float)a*b/c);
     return 0;
 }
