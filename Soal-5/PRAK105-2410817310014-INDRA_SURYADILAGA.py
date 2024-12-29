2944cf591011c19f703be5b043f259cf54436097
Indra Suryadilaga <indrasryd@gmail.com>
2024-12-29 17:20:36 +0800

    Update PRAK105-2410817310014-INDRA_SURYADILAGA.py

--- Soal-5/PRAK105-2410817310014-INDRA_SURYADILAGA.py
+++ Soal-5/PRAK105-2410817310014-INDRA_SURYADILAGA.py
@@ -1,12 +1,10 @@
-a=5
-b=6
-x=7
-y=8
+a = 5
+b = 6
+x = 7
+y = 8
 
-charvar = ['a', 'b', 'x', 'y']
-datavar = [a, b, x, y]
-
-for char, data in zip(charvar, datavar):
-    print(f'Variabel {char} bernilai {data}')
-
-print(f'Total sisa bagi dari a dibagi b dan x dibagi y adalah {(a%b)+(x%y)}')
+print(f"Variabel a bernilai {a}")
+print(f"Variabel b bernilai {b}")
+print(f"Variabel x bernilai {x}")
+print(f"Variabel y bernilai {y}")
+print(f"Total sisa bagi dari a dibagi b dan x dibagi y adalah {a % b+x % y}")
