063a2202cdd28d209926057d7d7b92d071691adc
Indra Suryadilaga <indrasryd@gmail.com>
2024-12-29 17:17:53 +0800

    Update PRAK102-2410817310014-INDRA_SURYADILAGA.py

--- Soal-2/PRAK102-2410817310014-INDRA_SURYADILAGA.py
+++ Soal-2/PRAK102-2410817310014-INDRA_SURYADILAGA.py
@@ -1,14 +1,8 @@
-# x = int(input("Masukkan nilai x: "))
-# y = int(input("Masukkan nilai y: "))
-# z = int(input("Masukkan nilai z: "))
-a=5
-b=6
-c=7
+a = 5
+b = 6
+c = 7
 
-charvar = ['a', 'b', 'c']
-datavar = [a, b, c]
-
-for char, data in zip(charvar, datavar):
-    print(f'Variabel {char} bernilai {data}')
-
-print(f'Jumlah dari a dikali  b dibagi c adalah {float(a*b/c):.4f}')
+print(f"variabel a bernilai {a}")
+print(f"variabel b bernilai {b}")
+print(f"variabel c bernilai {c}")
+print(f"Jumlah a dikali b dibagi c adalah {a*b/c}")
