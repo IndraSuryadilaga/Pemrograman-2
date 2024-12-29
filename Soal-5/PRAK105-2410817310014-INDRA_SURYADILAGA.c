34d9fd5f1bbde6a8b2f429badc11dd603356d7f9
Indra Suryadilaga <indrasryd@gmail.com>
2024-12-29 17:20:16 +0800

    Update PRAK105-2410817310014-INDRA_SURYADILAGA.c

--- Soal-5/PRAK105-2410817310014-INDRA_SURYADILAGA.c
+++ Soal-5/PRAK105-2410817310014-INDRA_SURYADILAGA.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    int a = 5;
-    int b = 6;
-    int x = 7;
-    int y = 8;
+    int a = 5, b = 6, x = 7, y = 8;
     
     printf("Variabel a bernilai %d\n",a);
     printf("Variabel b bernilai %d\n",b);
     printf("Variabel x bernilai %d\n",x);
     printf("Variabel y bernilai %d\n",y);
-    printf("Total sisa bagi dari a dibagi b dan x dibagi y adalah %d\n", (a%b)+(x%y));
+    printf("Total sisa bagi dari a dibagi b dan x dibagi y adalah %d", (a%b)+(x%y));
     return 0;
 }
